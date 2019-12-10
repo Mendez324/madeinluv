cd4042214a98ee4946a0b4f261b19c0e8030b290
André Mendes <andre.1993@hotmail.com>
2019-12-10 12:18:39 +0000

    gulp file converting, adding some footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,20 @@
-var gulp = require('gulp'),//http://gulpjs.com/
-util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
-sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
-autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
-minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
-rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
-log = util.log;
+"use strict";
 
-var sassFiles = "./scss/components/main.scss"
-
-gulp.task("sass", function() {
-  log("Generate CSS files " + (new Date()).toString());
-    gulp.src(sassFiles)
-		.pipe(sass({ style: 'expanded' }))
-					.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
-		.pipe(gulp.dest("css"))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('css'));
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var watch = require('gulp-watch');
+ 
+gulp.task('styles', function () {
+    return gulp
+        .src('scss/main.scss')
+        .pipe(sassGlob())
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
 });
 
-gulp.task("watch", function(){
-	log("Watching scss files for modifications");
-	gulp.watch(sassFiles, ["sass"]);
+gulp.task('watch', function () {
+    return watch('scss/components/**/*.scss', ['styles'])
+        
 });
 
-//gulp.task("default", ["sass"]);
